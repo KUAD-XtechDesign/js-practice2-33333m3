d58ccbe5a80bb4887588614530864a26d8f7b2d4
33333m3 <m-murashita0278@st.kyoto-art.ac.jp>
2020-11-09 01:54:20 +0900

    4

--- sample/js/script03.js
+++ sample/js/script03.js
@@ -1,14 +1,21 @@
 $(function(){
+
     let hako = 0;
 
+    $("#button01").on("click",function(){
+        for(let i=0; i < 10; i++){
+            hako = hako + 2;
+        }
+        $("#content").text(hako);
+    })
     
     $("#button02").on("click",function(){
         for(let i=0; i < 10; i++){
             hako = hako +i;
         }
         $("#content").text(hako);
-
     })
+
     $("#button3").on("click",function(){
         setInterval(function(){
             hako = hako + 1;
